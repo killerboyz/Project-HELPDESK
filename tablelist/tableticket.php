0ef0aef1085c56cbb63bd692114ac94a44265fe5
killerboyz <me@killerboyz.com>
2015-05-05 22:49:45 +0700

    TicketList
    TicketList doesn't split page yet.

--- tablelist/tableticket.php
+++ tablelist/tableticket.php
@@ -1,6 +1,68 @@
 <?php
 session_start();
 require "../function/function.php";
+include "../config/database.php";
+
+//$mysql = mysqlConnect();
+//$result = $mysql->query("SELECT TicketID, TicketTopic, TicketType, Priority, Status FROM ticket ORDER BY Status ASC , Priority DESC , TicketID ASC LIMIT 21");
+//$objResult = mysqli_fetch_assoc($mysql->query("SELECT * FROM ticket ORDER BY status ASC , Priority DESC"));
+
+function genTable()
+{
+	$mysql = mysqlConnect();
+	$result = $mysql->query("SELECT TicketID, TicketTopic, TicketType, Priority, Status FROM ticket ORDER BY Status ASC , Priority DESC , TicketID ASC ");
+
+	while ($row = mysqli_fetch_array($result, MYSQL_NUM)) 
+	{
+		echo "<form method='post' action='#'>\n";
+
+
+		if($row[3] == "High") echo "<tr class='danger'>\n";
+	    else if($row[3] == "Normal") echo "<tr class='warning'>\n";
+	    else echo "<tr class='active'>\n";
+
+	    echo "<td><h4>".$row[0]."</h></td>\n";
+	    ////////////////////////////////////////////////////////////////////////////////////////////////////////// TICKET ID
+	    echo "<td><h4>".$row[1]."</h></td>\n";
+	    ////////////////////////////////////////////////////////////////////////////////////////////////////////// TICKET TOPIC
+	    echo "<td>\n
+	    		<h4><span class='label label-primary'>".$row[2]."</span></h>\n
+	    		</td>\n";
+	    //////////////////////////////////////////////////////////////////////////////////////////////////////////	TYPE
+	    if($row[3] == "High") echo "<td>\n
+	    								<h4><span class='label label-danger'>High</span></h>\n
+									</td>\n";
+	    else if($row[3] == "Normal") echo "<td>\n
+	    										<h4><span class='label label-warning'>Normal</span></h>\n
+											</td>\n";
+	    else echo "<td>\n
+	    				<h4><span class='label label-info'>Low</span></h>\n
+					</td>\n";
+		////////////////////////////////////////////////////////////////////////////////////////////////////////// PRIORITY
+	    echo "<td>\n
+	    			<input class='btn btn-primary' type='submit' value='Click for Detail'></input>\n
+	    		</td>\n";
+	    ////////////////////////////////////////////////////////////////////////////////////////////////////////// TROUBLE DETAIL
+		if($row[4] == "Open") echo "<td>\n
+	    								<h4><span class='label label-info'>Open</span></h>\n
+	    							</td>\n";
+	    else if($row[4] == "Processing") echo "<td>\n
+	    											<h4><span class='label label-warning'>Processing</span></h>\n
+	    										</td>\n";
+	    else echo "<td>\n
+	    				<h4><span class='label label-default'>".$row[4]."</span>\n
+	    			</td>\n";
+	    ////////////////////////////////////////////////////////////////////////////////////////////////////////// STATUS
+
+	    echo "</tr>\n
+	    	</form>\n";
+	}
+}
+
+	
+
+
+
 
 ?>
 <!doctype html>
@@ -26,83 +88,23 @@ require "../function/function.php";
 	<!-- ---------------------------------------------------------------------------------------------------------------- NAVIGATOR BAR --------------------------------------------------------------------------------- -->
 
 
-	<div class="row">
-		<div class="col-md-push-1 col-md-10 col-md-pull-1">
-			<table class="table table-striped table-hover ">
-				<thead>
+	<div class="container">
+		<div class="row">
+			<table class='table table-striped table-hover'>
+			<thead>
 					<tr>
 						<th>TicketID</th>
 						<th>TicketTopic</th>
 						<th>Type</th>
 						<th>Priority</th>
 						<th>Trouble Detail</th>
+						<th>Status</th>
 
 					</tr>
 				</thead>
-				<tbody>
-					<tr class="info">
-						<td>1</td>
-						<td>Column content</td>
-						<td>
-							<span class="label label-success">Program A</span>
-						</td>
-						<td>
-							<span class="label label-danger">High</span>
-						</td>
-						<td>
-							<button aria-describedby="popover587911" title="" data-original-title="" type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="top" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">Detail</button>
 
-						</td>
-
-					</tr>
-					<tr class="success">
-						<td>2</td>
-						<td>Column content</td>
-						<td>
-							<span class="label label-success">Program A</span>
-						</td>
-						<td>
-							<span class="label label-danger">High</span>
-						</td>
-						<td>Click for detail</td>
-					</tr>
-					<tr class="danger">
-						<td>3</td>
-						<td>Column content</td>
-						<td>
-							<span class="label label-success">Program B</span>
-						</td>
-						<td>
-							<span class="label label-warning">Normal</span>
-						</td>
-						<td>Click for detail</td>
-					</tr>
-					<tr class="warning">
-						<td>4</td>
-						<td>Column content</td>
-						<td>
-							<span class="label label-success">Program B</span>
-						</td>
-						<td>
-							<span class="label label-warning">Normal</span>
-						</td>
-						<td>Click for detail</td>
-					</tr>
-					<tr class="active">
-						<td>5</td>
-						<td>Column content</td>
-						<td>
-							<span class="label label-success">Program C</span>
-						</td>
-						<td>
-							<span class="label label-info">Low</span>
-						</td>
-						<td>asd
-						</td>
-					</tr>
-				</tbody>
-			</table> 
-
+		<?php genTable();?>
+	<!-- ---------------------------------------------------------------------------------------------------------------- TABLE GENERATOR --------------------------------------------------------------------------------- -->
 		</div>
 	</div>
 
